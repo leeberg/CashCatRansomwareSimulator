97bd2c2fd21b175969096428d54f02d2a9786055
Lee Berg <leealanberg@gmail.com>
2018-11-12 18:42:48 -0600

    Code Push

---
+++ CashCat/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace CashCat
+{
+
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            
+
+            if (txtbox_Bitcoingaddess.Text == "123456789")
+            {
+                string Location = AppDomain.CurrentDomain.BaseDirectory;
+                DirectoryInfo d = new DirectoryInfo(Location);//Assuming Test is your Folder
+                FileInfo[] Files = d.GetFiles("*.locky"); //Getting Text files
+                string str = "";
+                foreach (FileInfo file in Files)
+                {
+                    Console.WriteLine(file.Name);
+                    string newfilename = (file.Name).Replace(".locky", ".txt");
+                    System.IO.File.Move(file.Name, newfilename);
+
+                }
+
+                MessageBox.Show("Unlocked! Thanks!", "You did it correct", MessageBoxButton.OK);
+            }
+            else
+            {
+                MessageBox.Show("Denied!", "You did it wrong", MessageBoxButton.OK);
+            }
+
+        }
+
+        private void txtbox_Bitcoingaddess_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txtbox_Bitcoingaddess.Text == "BITCOIN Address Here")
+            {
+                txtbox_Bitcoingaddess.Text = "";
+            }
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            string Location = AppDomain.CurrentDomain.BaseDirectory;
+            //Console.WriteLine("Processed file '{0}'.", Location);
+
+
+            DirectoryInfo d = new DirectoryInfo(Location);//Assuming Test is your Folder
+            FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
+            string str = "";
+            foreach (FileInfo file in Files)
+            {
+                Console.WriteLine(file.Name);
+                string newfilename = (file.Name).Replace(".txt", ".locky");
+                System.IO.File.Move(file.Name, newfilename);
+
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # "CashCat" The Ransomware Simulator
-A fun little ransomware simulator that will rename .TXT files to .LOCKY to simulate ransomware behavior for testing various monitoring tools.
+A fun little ransomware simulator for Windows that will rename .TXT files to .LOCKY to simulate ransomware behavior for testing various monitoring tools.
 
-THIS IS NOT A REAL RANSOMWARE! ALL IT DOES IS RENAME the extension of TXT FILES to .LOCKY 
+THIS IS **NOT** REAL RANSOMWARE! IT LITERALLY DOES NOT MATCH ANY REAL DEFINITION OF RANSOMWARE! ALL IT DOES IS RENAME files with the extension of .TXT to .LOCKY to test file activity monitoring tools.
 
+![](.\img\cashcat.jpg)
 
-
-Usage
+## Usage
 1. Place CashCat.exe in a directory with some .txt files in the same directory.
 2. Run Cashcast.exe.
 3. All .TXT files located in the same directory as Cashcat.exe will be renamed to .Locky - a common ransomware extension
 4. Enter the code 123456789 to rename all .LOCKY files to .TXT (this effectily UNLOCKS your files / resets your demo)
+
+
+![](.\img\CryptoLocker_Simulator.png)
+
+
+## Requirements
++ Tested on Windows 7, 10, Server 2012+
++ Requires at .NET 4.6.1
