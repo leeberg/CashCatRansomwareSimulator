f3698273c588dba8750f7d342603d3813b882f0c
Lee Berg <leeberg@outlook.com>
2019-12-17 18:08:23 -0600

    Prep for EnCrypt Operations

---
+++ CashCat/EncryptionOperation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NETCore.Encrypt;
+using System.IO;
+
+namespace CashCat
+{
+    class EncryptionOperation
+    {
+
+        //make this better - should make this "Easy mode"
+        private NETCore.Encrypt.Internal.AESKey aesKey = EncryptProvider.CreateAesKey();
+   
+        public void EncryptFile(string filepath)
+        {
+            var key = aesKey.Key;
+            var iv = aesKey.IV;
+
+            // TODO BACKUP MODE
+            string contents = File.ReadAllText(filepath);
+            string encrypted = EncryptProvider.AESEncrypt(contents, key);
+            File.WriteAllText(filepath, encrypted);
+      
+        }
+
+
+        public void DecryptFile(string filepath)
+        {
+            var key = aesKey.Key;
+            var iv = aesKey.IV;
+
+            string contents = File.ReadAllText(filepath);
+            string decrypted = EncryptProvider.AESDecrypt(contents, key);
+            File.WriteAllText(filepath, decrypted);
+
+            //TODO Error handling - you are out of luck screwed!
+
+        }
+
+
+    }
+}
