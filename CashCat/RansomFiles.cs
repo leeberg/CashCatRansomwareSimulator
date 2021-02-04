08cfa229f77f1f0bf5c822be3cc254d3c6e46bde
Lee Berg <leealanberg@gmail.com>
2021-02-04 16:22:25 -0600

    Update RansomFiles.cs
    people don't like swear words - someone please think of the children

--- CashCat/RansomFiles.cs
+++ CashCat/RansomFiles.cs
@@ -9,7 +9,7 @@ namespace CashCat
     class RansomFiles
     {
 
-        public string[] fileExtensions = new string[] { ".AngryDuck", ".0day", ".beef", ".beer", ".BitCryptor", ".BOMBO", ".CommonRansom", ".CoronaLock", ".Crypt0L0cker", ".D00mEd", ".Dablio", ".DIABLO6", ".FuckedByGhost",".fucku", ".filesfucked", ".GoldenEye", ".Horriblemorning", ".Lazarus", ".locker", ".locky", ".money",".newlock", ".NOOB", ".nuclear", ".parrot",".PayDay", ".petra", ".porno", ".PrOtOnIs", ".razor", ".Ryuk", ".satan", ".satana", ".SHARK", ".TROLL", ".weapologize", ".write_us_on_email", ".zeppelin" };
+        public string[] fileExtensions = new string[] { ".AngryDuck", ".0day", ".beef", ".beer", ".BitCryptor", ".BOMBO", ".CommonRansom", ".CoronaLock", ".Crypt0L0cker", ".D00mEd", ".Dablio", ".DIABLO6", ".pedro", ".FrozrLock", ".usam", ".GoldenEye", ".Horriblemorning", ".Lazarus", ".locker", ".locky", ".money",".newlock", ".NOOB", ".nuclear", ".parrot",".PayDay", ".petra", ".porno", ".PrOtOnIs", ".razor", ".Ryuk", ".satan", ".satana", ".SHARK", ".TROLL", ".weapologize", ".write_us_on_email", ".zeppelin" };
 
         private Random rand = new Random();
 
